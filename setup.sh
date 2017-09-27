cd86eaf1bbaca2b90512b5f1d5ec0dc8bb0a7f74
Bret Little <bret.little@gmail.com>
2017-09-27 10:07:11 -0600

    Add setup steps

--- setup.sh
+++ setup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-mkdir ~/tmp
+mkdir -p ~/tmp
 
 # install development tools
 sudo dnf groups install "Development Tools"
 sudo dnf groups install "C Development Tools and Libraries"
-sudo dnf install neovim guake tmux zsh
+sudo dnf install neovim guake tmux zsh xfce4-notes nm-applet shutter yumex-dnf dnf-plugins-core
+
+# setup docker
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf makecache
+sudo dnf install docker-ce
+sudo systemctl start docker
 
 # setup tmuxifier
 git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
@@ -14,23 +22,18 @@ git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
 nvm install node
 
+npm install -g yarn eslint babel-eslint eslint-plugin-react prettier
+
 # install i3
 sudo dnf install i3 i3status dmenu i3lock xbacklight feh conky scrot
 
 # install i3-gaps
 cd ~/tmp
-
-# clone the repository
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
-
-# compile & install
 autoreconf --force --install
 rm -rf build/
 mkdir -p build && cd build/
-
-# Disabling sanitizers is important for release versions!
-# The prefix and sysconfdir are, obviously, dependent on the distribution.
 ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
 make
 sudo make install
@@ -54,3 +57,5 @@ ln -s ~/.vim ~/.config/nvim
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 
 echo 'source "/home/blittle/.zsh.after"' >> /home/blittle/.zshrc
+
+rm -rf ~/tmp
